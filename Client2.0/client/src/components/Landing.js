695b73bc571221e39ff7cbd77f5da6e0ae039104
Jatin Bindra <jatinbindra171998@gmail.com>
2021-10-27 18:06:29 -0400

    dev

---
+++ Client2.0/client/src/components/Landing.js
@@ -0,0 +1,28 @@
+
+import '../App.css';
+import Button from 'react-bootstrap/Button';
+import { useState, useReducer } from 'react';
+
+
+function Landing() {
+
+  
+  return (
+    <div className="App">
+     
+<div className="main-design">
+
+<div className="card-here">
+
+</div>
+<div className="card-here2">
+    please view in mobile view
+</div>
+</div>
+   
+   
+    </div>
+  );
+}
+
+export default Landing;
