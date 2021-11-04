5f380581a4a8edcae3733d9c682113b220d8ef5c
Ashmi Mary Joseph <ashmijoseph@Ashmis-MacBook-Pro.local>
2021-11-04 14:05:37 -0400

    Integration of all locations with AR screen.

--- client/src/components/WindsorTourPlaces.js
+++ client/src/components/WindsorTourPlaces.js
@@ -74,25 +74,111 @@ class WindsorTourPlaces extends Component {
   constructor(props) {
     super(props);
     this.state = {};   
-    this.fireeventepicenter = this.fireeventepicenter.bind(this);
+    this.handleARScreen = this.handleARScreen.bind(this);
   }
 
-  fireeventepicenter() {
-    console.log((Math.abs(this.props.lat)-Math.abs(42.3111023))); 
-    console.log(Math.abs(this.props.long)-Math.abs(-83.0375344));
+  handleARScreen(loc) {
 
-    if(((Math.abs(this.props.lat)-Math.abs(42.30456195766808))>=0 && (Math.abs(this.props.lat)-Math.abs(42.30456195766808))<=0.001) || ((Math.abs(this.props.lat)-Math.abs(42.30456195766808))<=0 && (Math.abs(this.props.lat)-Math.abs(42.30456195766808))>=-(0.001))){
-      if(((Math.abs(this.props.long)-Math.abs(-83.0640182020208))>=0 && (Math.abs(this.props.long)-Math.abs(-83.0640182020208))<=0.001) || ((Math.abs(this.props.long)-Math.abs(-83.0640182020208))<=0 && (Math.abs(this.props.long)-Math.abs(-83.0640182020208))>=(-0.001))){
-        window.open("https://ashmi275.github.io/location-based-augumented-reality/index.html", "_blank")
+    const {lat, long} = this.props;
+
+    switch(loc){
+    case 'Alumni Hall and Conference Centre':
+
+          if(((Math.abs(lat)-Math.abs(42.302913607048254))>=0 && (Math.abs(lat)-Math.abs(42.302913607048254))<=0.001) || ((Math.abs(lat)-Math.abs(42.302913607048254))<=0 && (Math.abs(lat)-Math.abs(42.302913607048254))>=-(0.001))){
+            if(((Math.abs(long)-Math.abs(-83.06288937320598))>=0 && (Math.abs(long)-Math.abs(-83.06288937320598))<=0.001) || ((Math.abs(long)-Math.abs(-83.06288937320598))<=0 && (Math.abs(long)-Math.abs(-83.06288937320598))>=(-0.001))){
+              window.open("https://ashmi275.github.io/location-based-augumented-reality/alumni.html", "_blank")
+            }
+          }else{
+            window.alert(`You are not at ${loc} location!`);
+          }
+          break;
+
+    case 'Odette School of Business':
+
+      if(((Math.abs(lat)-Math.abs(42.3051739570509))>=0 && (Math.abs(lat)-Math.abs(42.3051739570509))<=0.001) || ((Math.abs(lat)-Math.abs(42.3051739570509))<=0 && (Math.abs(lat)-Math.abs(42.3051739570509))>=-(0.001))){
+        if(((Math.abs(long)-Math.abs(-83.06453655784131))>=0 && (Math.abs(long)-Math.abs(-83.06453655784131))<=0.001) || ((Math.abs(long)-Math.abs(-83.06453655784131))<=0 && (Math.abs(long)-Math.abs(-83.06453655784131))>=(-0.001))){
+          window.open("https://ashmi275.github.io/location-based-augumented-reality/odette.html", "_blank")
+        }
+      }else{
+        window.alert(`You are not at ${loc} location!`);
       }
-    }else{
-      window.alert("You are not at the EPI Center");
+      break;
+
+    case 'Leddy Library':
+
+      if(((Math.abs(lat)-Math.abs(42.30787102632674))>=0 && (Math.abs(lat)-Math.abs(42.30787102632674))<=0.001) || ((Math.abs(lat)-Math.abs(42.30787102632674))<=0 && (Math.abs(lat)-Math.abs(42.30787102632674))>=-(0.001))){
+        if(((Math.abs(long)-Math.abs(-83.06783345969251))>=0 && (Math.abs(long)-Math.abs(-83.06783345969251))<=0.001) || ((Math.abs(long)-Math.abs(-83.06783345969251))<=0 && (Math.abs(long)-Math.abs(-83.06783345969251))>=(-0.001))){
+          window.open("https://ashmi275.github.io/location-based-augumented-reality/leddy.html", "_blank")
+        }
+      }else{
+        window.alert(`You are not at ${loc} location!`);
+      }
+      break;
+
+    case 'Dillon Hall':
+
+      if(((Math.abs(lat)-Math.abs(42.30649720520779))>=0 && (Math.abs(lat)-Math.abs(42.30649720520779))<=0.001) || ((Math.abs(lat)-Math.abs(42.30649720520779))<=0 && (Math.abs(lat)-Math.abs(42.30649720520779))>=-(0.001))){
+        if(((Math.abs(long)-Math.abs(-83.06775508852849))>=0 && (Math.abs(long)-Math.abs(-83.06775508852849))<=0.001) || ((Math.abs(long)-Math.abs(-83.06775508852849))<=0 && (Math.abs(long)-Math.abs(-83.06775508852849))>=(-0.001))){
+          window.open("https://ashmi275.github.io/location-based-augumented-reality/dillon.html", "_blank")
+        }
+      }else{
+        window.alert(`You are not at ${loc} location!`);
+      }
+      break;
+
+    case 'Essex Hall':
+
+      if(((Math.abs(lat)-Math.abs(42.30509546137389))>=0 && (Math.abs(lat)-Math.abs(42.30509546137389))<=0.001) || ((Math.abs(lat)-Math.abs(42.30509546137389))<=0 && (Math.abs(lat)-Math.abs(42.30509546137389))>=-(0.001))){
+        if(((Math.abs(long)-Math.abs(-83.06675840202075))>=0 && (Math.abs(long)-Math.abs(-83.06675840202075))<=0.001) || ((Math.abs(long)-Math.abs(-83.06675840202075))<=0 && (Math.abs(long)-Math.abs(-83.06675840202075))>=(-0.001))){
+          window.open("https://ashmi275.github.io/location-based-augumented-reality/essex.html", "_blank")
+        }
+      }else{
+        window.alert(`You are not at ${loc} location!`);
+      }
+      break;
+
+    case 'St. Denis Athletic & Community Centre':
+
+      if(((Math.abs(lat)-Math.abs(42.29924028596501))>=0 && (Math.abs(lat)-Math.abs(42.29924028596501))<=0.001) || ((Math.abs(lat)-Math.abs(42.29924028596501))<=0 && (Math.abs(lat)-Math.abs(42.29924028596501))>=-(0.001))){
+        if(((Math.abs(long)-Math.abs(-83.06141230387232))>=0 && (Math.abs(long)-Math.abs(-83.06141230387232))<=0.001) || ((Math.abs(long)-Math.abs(-83.06141230387232))<=0 && (Math.abs(long)-Math.abs(-83.06141230387232))>=(-0.001))){
+          window.open("https://ashmi275.github.io/location-based-augumented-reality/denis.html", "_blank")
+        }
+      }else{
+        window.alert(`You are not at ${loc} location!`);
+      }
+      break;
+
+    case 'Turtle Island Walk':
+
+      if(((Math.abs(lat)-Math.abs(42.302913607048254))>=0 && (Math.abs(lat)-Math.abs(42.302913607048254))<=0.001) || ((Math.abs(lat)-Math.abs(42.302913607048254))<=0 && (Math.abs(lat)-Math.abs(42.302913607048254))>=-(0.001))){
+        if(((Math.abs(long)-Math.abs(-83.06288937320598))>=0 && (Math.abs(long)-Math.abs(-83.06288937320598))<=0.001) || ((Math.abs(long)-Math.abs(-83.06288937320598))<=0 && (Math.abs(long)-Math.abs(-83.06288937320598))>=(-0.001))){
+          window.open("https://ashmi275.github.io/location-based-augumented-reality/turtle.html", "_blank")
+        }
+      }else{
+        window.alert(`You are not at ${loc} location!`);
+      }
+      break;
+
+    case 'EPICentre':
+
+      if(((Math.abs(lat)-Math.abs(42.30456195766808))>=0 && (Math.abs(lat)-Math.abs(42.30456195766808))<=0.001) || ((Math.abs(lat)-Math.abs(42.30456195766808))<=0 && (Math.abs(lat)-Math.abs(42.30456195766808))>=-(0.001))){
+        if(((Math.abs(long)-Math.abs(-83.0640182020208))>=0 && (Math.abs(long)-Math.abs(-83.0640182020208))<=0.001) || ((Math.abs(long)-Math.abs(-83.0640182020208))<=0 && (Math.abs(long)-Math.abs(-83.0640182020208))>=(-0.001))){
+          window.open("https://ashmi275.github.io/location-based-augumented-reality/index.html", "_blank")
+        }
+      }else{
+        window.alert(`You are not at ${loc} location!`);
+      }
+      break;
+
+    default:
+      window.alert("No data avaliable. Try Again!");
     }
+
+
   }
   
   
   render() {
-    const {lat, long} = this.props;
 
     return (
       <div >
@@ -106,7 +192,7 @@ class WindsorTourPlaces extends Component {
             </PlaceCategory>
             {locations.map((loc, index) => {
                 return (
-                    <Locations key={index} onClick={this.fireeventepicenter}>
+                    <Locations key={index} onClick={() => this.handleARScreen(loc)}>
                         <LocationIcon> 
                         <i className="small material-icons">location_on</i>
                         </LocationIcon>  
